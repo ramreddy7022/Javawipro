8f801ff20b32666c263dc0ffb93251c4fcac8d93
P S HINDU <167954781+ramreddy7022@users.noreply.github.com>
2025-07-29 15:29:08 +0530

    Add files via upload

---
+++ javawipro/src/abstraction/Tiger.java
@@ -0,0 +1,9 @@
+package abstraction;
+
+class Tiger extends Animal {
+	 public void  sound() {
+		 System.out.println("Tiger roars");
+	 }
+	
+
+}
---
+++ javawipro/src/abstraction/lion.java
@@ -0,0 +1,11 @@
+package abstraction;
+
+class lion extends Animal{
+	 public void sound() {
+	        // Print "Lion roars!" to the console
+	        System.out.println("Lion roars!");
+	 }
+
+}
+
+
