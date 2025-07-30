0ac5c2f7dec694429c2fecb29ea28753f1fc9110
P S HINDU <167954781+ramreddy7022@users.noreply.github.com>
2025-07-30 11:14:53 +0530

    Add files via upload

---
+++ javawipro/src/day1/Demo3.java
@@ -0,0 +1,19 @@
+package day1;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		int a = 10;
+
+		int b = 20;
+
+		System.out.println("++a ="+(++a));
+
+	
+
+		System.out.println("--b ="+(--b));
+// TODO Auto-generated method stub
+
+	}
+
+}
